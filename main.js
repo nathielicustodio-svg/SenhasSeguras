7e47610c6ab59b5cd8cfe8198dc394e7d2801a67
nathieli <nathieli.custodio@escola.pr.gov.br>
2026-07-07 17:20:30 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
-let tamanhoSenha = 12;
+let tamanhoSenha = 5;
 numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
@@ -12,16 +12,29 @@ function diminuiTamanho(){
        tamanhoSenha--;
    }
    numeroSenha.textContent = tamanhoSenha;
+   geraSenha();
 }
 function aumentaTamanho(){
     if (tamanhoSenha < 20){
         tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 
+const campoSenha = document.querySelector('#campo-senha');
 
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+geraSenha();
+campoSenha.value = letrasMaiusculas; 
 
-
-
+function geraSenha(){
+    let senh = '';
+    for (let i = o; < tamanhoSenha;i++){
+    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+    numeroAleatorio = Math.floor(numeroAleatorio);
+    senha = senha + letrasMaiusculas[mumeroAleatorio];
+}
+campoSenha.value = letrasMaiusculas;
+}
 
